f3f30954638e4e541a3d1f42f6d1c0fb7b0b1222
Cedrick-CL <licedrick72@gmail.com>
2026-02-11 13:53:17 +0100

    Update pyboolnet.py

--- format/pyboolnet.py
+++ format/pyboolnet.py
@@ -1,22 +1,4 @@
 import pylfit
-data = [ \
-(["0","0","0"],["0","0","1"]), \
-(["1","0","0"],["0","0","0"]), \
-(["0","1","0"],["1","0","1"]), \
-(["0","0","1"],["0","0","1"]), \
-(["1","1","0"],["1","0","0"]), \
-(["1","0","1"],["0","1","0"]), \
-(["0","1","1"],["1","0","1"]), \
-(["1","1","1"],["1","1","0"])]
-
-dataset = pylfit.preprocessing.discrete_state_transitions_dataset_from_array(data=data, feature_names=["p_t_1","q_t_1","r_t_1"], target_names=["p_t","q_t","r_t"])
-
-model = pylfit.models.DMVLP(features=dataset.features, targets=dataset.targets)
-model.compile(algorithm="pride") 
-
-model.fit(dataset=dataset)
-model.summary()
-
 
 def modelToPyboolnet(model, outputFile = "./output"):
     variable = [i[0][:-2] for i in  model.targets]
@@ -50,5 +32,6 @@ def modelToPyboolnet(model, outputFile = "./output"):
             if countOrRules == 0:
                 f.write("1")
             f.write("\n")
+
 
 modelToPyboolnet(model)
