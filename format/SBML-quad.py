1cd5cf147b79a72d0cc79f42a0875b827fd7240d
Cedrick-CL <licedrick72@gmail.com>
2026-02-11 13:52:37 +0100

    Update SBML-quad.py

--- format/SBML-quad.py
+++ format/SBML-quad.py
@@ -62,23 +62,3 @@ def modelToSbmlQual(model, outputFile="./output_model.sbml"):
         default_term.setResultLevel(0)
 
     libsbml.writeSBMLToFile(doc, outputFile)
-
-data = [ \
-(["0","0","0"],["0","0","1"]), \
-(["1","0","0"],["0","0","0"]), \
-(["0","1","0"],["1","0","1"]), \
-(["0","0","1"],["0","0","1"]), \
-(["1","1","0"],["1","0","0"]), \
-(["1","0","1"],["0","1","0"]), \
-(["0","1","1"],["1","0","1"]), \
-(["1","1","1"],["1","1","0"])]
-
-dataset = pylfit.preprocessing.discrete_state_transitions_dataset_from_array(data=data, feature_names=["p_t_1","q_t_1","r_t_1"], target_names=["p_t","q_t","r_t"])
-
-model = pylfit.models.DMVLP(features=dataset.features, targets=dataset.targets)
-model.compile(algorithm="pride") 
-
-model.fit(dataset=dataset)
-model.summary()
-
-modelToSbmlQual(model)
